2947c64cf62a6381ba91e8ac3a42779ed1094d15
mzackk <mzackry3@gmail.com>
2024-03-12 11:17:45 +0800

    Added Validation on the Login Form

--- myBlog/resources/views/auth/login.blade.php
+++ myBlog/resources/views/auth/login.blade.php
@@ -5,16 +5,25 @@
 @endsection
 
 @section('content')
-<form method="POST" action="#">
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
     <div class="form-group">
        <label class="small mb-1" for="input_login_email">Email</label>
-       <input name="email" class="form-control py-4" id="input_login_email" type="email" placeholder="Enter email address" autocomplete="email"/>
-       <!-- todo: show message validation (email) -->
+       <input name="email" class="form-control py-4 @error('email') is-invalid @enderror" id="input_login_email" type="email" placeholder="Enter email address" value="{{ old('email') }}" autocomplete="email"/>
+       @error('email')
+       <span class="invalid-feedback" role="alert">
+           <strong>{{ $message }}</strong>
+       </span>
+   @enderror
     </div>
     <div class="form-group">
        <label class="small mb-1" for="input_login_password">Password</label>
-       <input name="password" class="form-control py-4" id="input_login_password" type="password" placeholder="Enter password" autocomplete="current-password" />
-       <!-- todo: show message validation (password) -->
+       <input name="password" class="form-control py-4 @error('password') is-invalid @enderror" id="input_login_password" type="password" placeholder="Enter password" autocomplete="current-password" />
+       @error('password')
+       <span class="invalid-feedback" role="alert">
+           <strong>{{ $message }}</strong>
+       </span>
+   @enderror
     </div>
     <div class="form-group d-flex align-items-center justify-content-between mt-4 mb-0">
        <a class="small" href="#">Forgot Password?</a>
@@ -22,9 +31,6 @@
     </div>
  </form>
 
- <!-- for message validation -->
- <span class="invalid-feedback" role="alert">
-    <strong>message</strong>
- </span>
+
 
 @endsection
--- myBlog/resources/views/layouts/auth.blade.php
+++ myBlog/resources/views/layouts/auth.blade.php
@@ -6,7 +6,7 @@
    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
    <title>
-      {{ config('app_name') }} - @yield('title')
+      {{ config('app.name') }} - @yield('title')
    </title>
    <link rel="stylesheet" href="{{ asset('vendor/my-auth/css/auth.css') }}">
 
