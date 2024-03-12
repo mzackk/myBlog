7a53a13daafd89ec668750ee2d6ba5132af624c9
mzackk <mzackry3@gmail.com>
2024-03-12 13:38:12 +0800

    Fix redirect authentication login

---
+++ myBlog/resources/views/dashboard/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.dashboard')
+
+@section('title')
+    Dashboard Main Page
+@endsection
+
+@section('breadcrumbs')
+    Breadcrumbs
+@endsection
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <h2>Welcome To The Page {{ Auth::user() -> name}}</h2>
+        </div>
+    </div>
+@endsection
