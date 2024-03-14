b4e33a60bf79899aa1a5dab3ff015eb1b2247e75
mzackk <mzackry3@gmail.com>
2024-03-14 11:30:21 +0800

    - Added layout for add in the categories - Added route to access the "add" - Added localization for the MY and EN

--- myBlog/resources/lang/my/categories.php
+++ myBlog/resources/lang/my/categories.php
@@ -1,97 +1,97 @@
 <?php
 /*
-language : Malaysia
+language : Malay
 */
 return [
-    'title' => [
-        'index' => 'Kategori',
-        'create' => 'Tambah kategori',
-        'edit' => 'Ubah kategori',
-        'detail' => 'Detail kategori',
+  'title' => [
+    'index' => 'Kategori',
+    'create' => 'Tambah kategori',
+    'edit' => 'Ubah kategori',
+    'detail' => 'Detail kategori',
+  ],
+  'form_control' => [
+    'input' => [
+      'title' => [
+        'label' => 'Tajuk',
+        'placeholder' => 'Masukkan tajuk',
+        'attribute' => 'tajuk'
+      ],
+      'slug' => [
+        'label' => 'Slug',
+        'placeholder' => 'Dibuat secara automatik',
+        'attribute' => 'slug'
+      ],
+      'thumbnail' => [
+        'label' => 'Thumbnail',
+        'placeholder' => 'Carian thumbnail',
+        'attribute' => 'thumbnail'
+      ],
+      'search' => [
+        'label' => 'Carian',
+        'placeholder' => 'Cari kategori',
+        'attribute' => 'carian'
+      ]
     ],
-    'form_control' => [
-        'input' => [
-            'title' => [
-                'label' => 'Judul',
-                'placeholder' => 'Masukan judul',
-                'attribute' => 'judul'
-            ],
-            'slug' => [
-                'label' => 'Slug',
-                'placeholder' => 'Automatis dibuatkan',
-                'attribute' => 'slug'
-            ],
-            'thumbnail' => [
-                'label' => 'Thumbnail',
-                'placeholder' => 'Telusuri thumbnail',
-                'attribute' => 'thumbnail'
-            ],
-            'search' => [
-                'label' => 'Pencarian',
-                'placeholder' => 'Cari kategori',
-                'attribute' => 'pencarian'
-            ]
-        ],
-        'select' => [
-            'parent_category' => [
-                'label' => 'Induk kategori',
-                'placeholder' => 'Pilih induk kategori',
-                'attribute' => 'induk kategori'
-            ]
-        ],
-        'textarea' => [
-            'description' => [
-                'label' => 'Deskripsi',
-                'placeholder' => 'Masukan deskripsi',
-                'attribute' => 'deskripsi'
-            ],
-        ]
+    'select' => [
+      'parent_category' => [
+        'label' => 'Kategori induk',
+        'placeholder' => 'Pilih kategori induk',
+        'attribute' => 'kategori induk'
+      ]
     ],
-    'button' => [
-        'create' => [
-            'value' => 'Tambah'
-        ],
-        'save' => [
-            'value' => 'Simpan'
-        ],
-        'edit' => [
-            'value' => 'Ubah'
-        ],
-        'delete' => [
-            'value' => 'Hapus'
-        ],
-        'cancel' => [
-            'value' => 'Batal'
-        ],
-        'browse' => [
-            'value' => 'Telusuri'
-        ],
-        'back' => [
-            'value' => 'Kembali'
-        ],
-    ],
-    'alert' => [
-        'create' => [
-            'title' => "Tambah kategori",
-            'message' => [
-                'success' => "Kategori berhasil disimpan.",
-                'error' => "Terjadi kesalahan saat menyimpan kategori. :error"
-            ]
-        ],
-        'update' => [
-            'title' => 'Ubah kategori',
-            'message' => [
-                'success' => "Kategori berhasil diperbaharui.",
-                'error' => "Terjadi kesalahan saat perbarui kategori. :error"
-            ]
-        ],
-        'delete' => [
-            'title' => 'Hapus kategori',
-            'message' => [
-                'confirm' => "Anda pati untuk menghapus kategori :title ?",
-                'success' => "Kategori berjaya dihapus",
-                'error' => "Error saat menghapus kategori. :error"
-            ]
-        ],
+    'textarea' => [
+      'description' => [
+        'label' => 'Penerangan',
+        'placeholder' => 'Masukkan penerangan',
+        'attribute' => 'penerangan'
+      ],
     ]
+  ],
+  'button' => [
+    'create' => [
+      'value' => 'Tambah'
+    ],
+    'save' => [
+      'value' => 'Simpan'
+    ],
+    'edit' => [
+      'value' => 'Ubah'
+    ],
+    'delete' => [
+      'value' => 'Hapus'
+    ],
+    'cancel' => [
+      'value' => 'Batal'
+    ],
+    'browse' => [
+      'value' => 'Carian'
+    ],
+    'back' => [
+      'value' => 'Kembali'
+    ],
+  ],
+  'alert' => [
+    'create' => [
+      'title' => "Tambah kategori",
+      'message' => [
+        'success' => "Kategori berjaya disimpan.",
+        'error' => "Terjadi ralat semasa menyimpan kategori. :error"
+      ]
+    ],
+    'update' => [
+      'title' => 'Ubah kategori',
+      'message' => [
+        'success' => "Kategori berjaya dikemas kini.",
+        'error' => "Terjadi ralat semasa mengemaskini kategori. :error"
+      ]
+    ],
+    'delete' => [
+      'title' => 'Hapus kategori',
+      'message' => [
+        'confirm' => "Yakin untuk menghapus kategori :title ?",
+        'success' => "Kategori berjaya dihapus",
+        'error' => "Terjadi ralat semasa menghapus kategori. :error"
+      ]
+    ],
+  ]
 ];
