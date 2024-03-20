02c1b2d68044c65c6d2271a134b1894ce5b09d1c
mzackk <mzackry3@gmail.com>
2024-03-20 10:51:03 +0800

    Adding validation for adding tags

--- myBlog/resources/views/tags/create.blade.php
+++ myBlog/resources/views/tags/create.blade.php
@@ -13,23 +13,39 @@
     <div class="col-md-12">
        <div class="card">
           <div class="card-body">
-             <form action="" method="POST">
+             <form action="{{ route('tags.store') }}" method="POST">
+               @csrf
                 <!-- title -->
                 <div class="form-group">
                    <label for="input_tag_title" class="font-weight-bold">
                       {{ trans('tags.form_control.input.title.label') }}
                    </label>
-                   <input id="input_tag_title" value="" name="title" type="text"
-                      class="form-control"
+                   <input id="input_tag_title" value="{{ old('title') }}" name="title" type="text"
+                      class="form-control @error('title')
+                         is-invalid
+                      @enderror"
                       placeholder="{{ trans('tags.form_control.input.title.placeholder') }}" />
+                      @error('title')
+                        <span class="invalid-feedback">
+                           <strong>{{ $message }}</strong>
+                        </span>
+                      @enderror
                 </div>
                 <!-- slug -->
                 <div class="form-group">
                    <label for="input_tag_slug" class="font-weight-bold">
                       {{ trans('tags.form_control.input.slug.label') }}
                    </label>
-                   <input id="input_tag_slug" value="" name="slug" type="text" class="form-control" placeholder="{{ trans('tags.form_control.input.slug.placeholder') }}" readonly />
+                   <input id="input_tag_slug" value="{{ old('slug') }}" name="slug" type="text" class="form-control
+                   @error('slug') is-invalid @enderror"
+                   placeholder="{{ trans('tags.form_control.input.slug.placeholder') }}" readonly />
+                     @error('slug')
+                        <span class="invalid-feedback">
+                           <strong>{{ $message }}</strong>
+                        </span>
+                      @enderror
                 </div>
+
                 <button type="submit" class="btn btn-primary float-right px-4">
                     {{ trans('tags.button.save.value') }}
                  </button>
