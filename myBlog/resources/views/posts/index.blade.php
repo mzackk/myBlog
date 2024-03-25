be0e3d83a6573ba8495640ddd8fee385e3e23e12
mzackk <mzackry3@gmail.com>
2024-03-25 12:13:25 +0800

    Displaying data post addition

--- myBlog/resources/views/posts/index.blade.php
+++ myBlog/resources/views/posts/index.blade.php
@@ -54,7 +54,42 @@
           </div>
           <div class="card-body">
              <ul class="list-group list-group-flush">
-                <!-- list post -->
+                {{-- list- post --}}
+                @forelse ($posts as $post)
+                <div class="card">
+                  <div class="card-body">
+                  <h5>
+                     {{ $post->title }}
+                  </h5>
+                     <p>
+                     {{ $post->description }}
+                     </p>
+                     <div class="float-right">
+                        <!-- detail -->
+                        <a href="#" class="btn btn-sm btn-primary" role="button">
+                           <i class="fas fa-eye"></i>
+                        </a>
+                        <!-- edit -->
+                        <a class="btn btn-sm btn-info" role="button">
+                           <i class="fas fa-edit"></i>
+                        </a>
+                        <!-- delete -->
+                        <form class="d-inline" action="" method="POST">
+                           <button type="submit" class="btn btn-sm btn-danger">
+                              <i class="fas fa-trash"></i>
+                           </button>
+                        </form>
+                     </div>
+                  </div>
+               </div>
+               
+                @empty
+                    <p>
+                     <strong>
+                        {{ trans('posts.label.no_data.fetch') }}
+                     </strong>
+                    </p>
+                @endforelse
              </ul>
           </div>
        </div>
