53c274ccc9446dcec8797ada41006f1868146117
mzackk <mzackry3@gmail.com>
2024-04-03 16:00:39 +0800

    Update RoleController and index view to implement search functionality
    The RoleController index function has been updated to accept a Request object and perform a search if a keyword is present. The index view has also been updated to include a search form and display search results.

--- myBlog/app/Http/Controllers/RoleController.php
+++ myBlog/app/Http/Controllers/RoleController.php
@@ -13,10 +13,16 @@ class RoleController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
+        $roles = [];
+        if($request-> has('keyword')){
+            $roles= Role::where('name', 'LIKE', "%{$request->keyword}%")->get();
+        }else{
+            $roles = Role::all();
+        }
         return view('roles.index',[
-            'roles' => Role::all()
+            'roles' => $roles
         ]);
     }
 
