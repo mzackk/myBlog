0c14a703d97ea4a42dc72954f5004630f47c018f
mzackk <mzackry3@gmail.com>
2024-04-04 11:41:55 +0800

    Displaying user data
    The UserController's index method has been updated to pass all users to the view using the `User::all()` method. Additionally, the `users.index` view has been modified to display a list of users using a `@forelse` loop. Each user is displayed in a separate card containing their name, email, and role. A delete button and an edit button are also included for each user. If no users are found, a message is displayed indicating that there is no data to fetch.

--- myBlog/resources/views/users/index.blade.php
+++ myBlog/resources/views/users/index.blade.php
@@ -36,7 +36,70 @@
           </div>
           <div class="card-body">
              <div class="row">
-                <!-- list users -->
+                <!-- list users:start -->
+                @forelse ($users as $user)
+                <div class="col-md-4">
+                    <div class="card my-1">
+                       <div class="card-body">
+                          <div class="row">
+                             <div class="col-md-2">
+                                <i class="fas fa-id-badge fa-5x"></i>
+                             </div>
+                             <div class="col-md-10">
+                                <table>
+                                   <tr>
+                                      <th>
+                                         {{ trans('users.label.name') }}
+                                      </th>
+                                      <td>:</td>
+                                      <td>
+                                         {{ $user->name }}
+                                      </td>
+                                   </tr>
+                                   <tr>
+                                      <th>
+                                         {{ trans('users.label.email') }}
+                                      </th>
+                                      <td>:</td>
+                                      <td>
+                                         {{ $user->email }}
+                                      </td>
+                                   </tr>
+                                   <tr>
+                                      <th>
+                                         {{ trans('users.label.role') }}
+                                      </th>
+                                      <td>:</td>
+                                      <td>
+                                         {{ $user->roles->first()->name }}
+                                      </td>
+                                   </tr>
+                                </table>
+                             </div>
+                          </div>
+                          <div class="float-right">
+                             <!-- edit -->
+                             <a href="" class="btn btn-sm btn-info" role="button">
+                                <i class="fas fa-edit"></i>
+                             </a>
+                             <!-- delete -->
+                             <form action="" method="POST" role="alert" class="d-inline">
+                                <button type="submit" class="btn btn-sm btn-danger">
+                                   <i class="fas fa-trash"></i>
+                                </button>
+                             </form>
+                          </div>
+                       </div>
+                    </div>
+                 </div>
+                @empty
+                    <p>
+                        <strong>
+                            {{ trans('users.label.no_data.fetch') }}
+                        </strong>
+                    </p>
+                @endforelse
+                <!-- list users:end -->
              </div>
           </div>
           <div class="card-footer">
